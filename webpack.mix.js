613c17873ab40437e3da7567ed7a907bdbab563a
Alexis <azen0x.alexis@gmail.com>
2021-03-18 09:26:23 +0100

    Remove app.css support

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/style.scss', 'public/css')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/style.scss', 'public/css');
