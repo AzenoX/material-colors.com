d86664442cdbf146603291cb6af225b5c213daed
Alexis Hayat <alexis.hayat@gmail.com>
2023-10-05 23:32:09 +0200

    fix: bugs after version bump

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -8,13 +8,11 @@ use Illuminate\Support\Facades\Route;
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
 
-Route::prefix(config('backpack.base.route_prefix', 'admin'))->middleware(array_merge((array) config('backpack.base.web_middleware', 'web'), (array) config('backpack.base.middleware_key', 'admin')))->middleware(array_merge((array) config('backpack.base.web_middleware', 'web'), (array) config('backpack.base.middleware_key', 'admin')))->group([
-    'middleware' => array_merge(
-        (array) config('backpack.base.web_middleware', 'web'),
-        (array) config('backpack.base.middleware_key', 'admin')
-    ), ], function () { // custom admin routes
-        Route::crud('custom-palettes', 'CustomPalettesCrudController');
-        Route::crud('gradient', 'GradientCrudController');
-        Route::crud('socials', 'SocialsCrudController');
-        Route::crud('user', 'UserCrudController');
+Route::prefix(config('backpack.base.route_prefix', 'admin'))
+    ->middleware(array_merge((array) config('backpack.base.web_middleware', 'web'), (array) config('backpack.base.middleware_key', 'admin')))
+    ->group(function () { // custom admin routes
+        Route::crud('custom-palettes', 'App\Http\Controllers\Admin\CustomPalettesCrudController');
+        Route::crud('gradient', 'App\Http\Controllers\Admin\GradientCrudController');
+        Route::crud('socials', 'App\Http\Controllers\Admin\SocialsCrudController');
+        Route::crud('user', 'App\Http\Controllers\Admin\UserCrudController');
     }); // this should be the absolute last line of this file
